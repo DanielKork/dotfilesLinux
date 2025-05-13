8e5cbd93e3bb8840fd65b1656fea969136f4a84d
Daniel Korkus <danelkor@gmail.com>
2025-05-13 22:16:44 +0300

    fzf changes to zsh

--- fzf/fzf.bash
+++ fzf/fzf.bash
@@ -1,2 +1,3 @@
 # Load FZF key bindings if installed via apt
-[ -f /usr/share/doc/fzf/examples/key-bindings.bash ] && source /usr/share/doc/fzf/examples/key-bindings.bash
+#[ -f /usr/share/doc/fzf/examples/key-bindings.bash ] && source /usr/share/doc/fzf/examples/key-bindings.bash
+[ -f /usr/share/doc/fzf/examples/key-bindings.zsh ] && source /usr/share/doc/fzf/examples/key-bindings.zsh
